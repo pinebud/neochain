b6dd15c16b59390eb0c3c500414517d2ba62ec44
Jing Ning <ningjing@huobi.com>
2018-12-10 11:51:27 +0800

    Complete creation and finding functions for mongo

--- mongoclient.py
+++ mongoclient.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-
+import printjson as printjson
 import pymongo
 import time
+import pprint
 
 
 class MongoClient(object):
 
     def __init__(self):
-        self.myclient = pymongo.MongoClient("mongodb://sense:123456@47.92.3.134:27017/")
+        self.myclient = pymongo.MongoClient('47.92.3.134', 27017)
         self.mydb = self.myclient["neochain"]
+        self.mydb.authenticate('sense', '123456')
 
     def insert(self, value):
         self.mydb.get_collection('neochain').insert_one({'value': value, 'created_time': time.time()})
 
-    def find_latest(self, name):
-        return self.mydb[name].find().sort('created_time', direction=pymongo.DESCENDING).limit(1)
+    def find_latest(self):
+        return self.mydb.get_collection('neochain').find().sort('created_time',
+                                                                direction=pymongo.DESCENDING).limit(1)
 
 
 client = MongoClient()
-client.insert({'test': '001'})
+# client.insert({'test': '001'})
 # client.insert({'test': '002'})
-print(client.find_latest())
+result = client.find_latest()
+for item in result:
+    pprint.pprint(item)
